79475926d65680f32a838ea583e12190e5b8d21e
pablogormi <pablogormi822@gmail.com>
2024-11-28 16:04:46 +0100

    Change GUI

--- src/client/gui/ConfigGUI.java
+++ src/client/gui/ConfigGUI.java
@@ -13,6 +13,8 @@ import javax.swing.JSplitPane;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+
 import java.awt.GridLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.FlowLayout;
@@ -23,6 +25,10 @@ import javax.swing.JTextArea;
 import java.awt.Font;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.DropMode;
 
 public class ConfigGUI extends JFrame {
 
@@ -34,6 +40,13 @@ public class ConfigGUI extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -100,23 +113,36 @@ public class ConfigGUI extends JFrame {
 //		});
 		JPanel panelConfig = new JPanel();
 		panel.add(panelConfig, "CONFIG");
-		panelConfig.setLayout(new GridLayout(0, 2, 0, 0));
+		GridBagLayout gbl_panelConfig = new GridBagLayout();
+		gbl_panelConfig.columnWidths = new int[] {142, 0, 0};
+		gbl_panelConfig.rowHeights = new int[]{28, 0};
+		gbl_panelConfig.columnWeights = new double[]{0.0, 0.0, 0.0};
+		gbl_panelConfig.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelConfig.setLayout(gbl_panelConfig);
 		
-		JPanel panelLabel = new JPanel();
-		panelConfig.add(panelLabel);
-		
-		JLabel lblNewLabel = new JLabel("Directorio sincronizado");
-		panelLabel.add(lblNewLabel);
-		
-		JPanel panelOpt = new JPanel();
-		panelConfig.add(panelOpt);
+		JLabel lblNewLabel = new JLabel("Ruta de carpeta compartida");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panelConfig.add(lblNewLabel, gbc_lblNewLabel);
 		
 		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		panelConfig.add(textField, gbc_textField);
 		textField.setColumns(10);
-		panelOpt.add(textField);
 		
 		JButton btnNewButton = new JButton("Buscar");
-		panelOpt.add(btnNewButton);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 0;
+		panelConfig.add(btnNewButton, gbc_btnNewButton);
 		JPanel panelLog = new JPanel();
 		panel.add(panelLog, "LOG");
 		panelLog.setLayout(new GridLayout(0, 1, 0, 0));
@@ -126,5 +152,7 @@ public class ConfigGUI extends JFrame {
 		txtrsObtenidoRegistro.setFont(new Font("Monospaced", Font.PLAIN, 10));
 		txtrsObtenidoRegistro.setText("1s: Obtenido registro del servidor\r\n5s: Obteniendo archivo de cliente 1.1.1.1\r\n10s: Finalizada actualización\r\n");
 		panelLog.add(txtrsObtenidoRegistro);
+		
+		this.pack();
 	}
 }
